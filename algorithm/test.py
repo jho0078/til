fecf1187ab68663a0dd4a08f6a54106c13a515d7
jho0078 <jho0078.jho0078@gmail.com>
2019-01-18 17:58:29 +0900

    D13 | 190118 PM | make gitignore

--- algorithm/test.py
+++ algorithm/test.py
@@ -1,15 +1,2 @@
-def my_languages(dic):
-    key_list = []
-    for value in dic.values():
-        if value >= 60:
-            key_list.append(value)
-    key_list.sort(reverse=True)
-
-    result = []
-    for i in key_list:
-        for key, value in dic.items():
-            if i == value:
-                result.append(key)
-    return result
-
-print(my_languages({"Java": 10, "Ruby": 67, "Python": 89, "PHP": 61}))
+{"movieInfoResult":{"movieInfo":{"movieCd":"20184105","movieNm":"말모이","movieNmEn":"MAL·MO·E: The Secret Mission","movieNmOg":"","showTm":"135","prdtYear":"2018","openDt":"20
+190109","prdtStatNm":"개봉","typeNm":"장편","nations":[{"nationNm":"한국"}],"genres":[{"genreNm":"드라마"}],"directors":[{"peopleNm":"엄유나","peopleNmEn":"EOM Yuna"}],"actors":[{"peopleNm":"유해진","peopleNmEn":"YOO Hae-jin","cast":"","castEn":""},{"peopleNm":"윤계상","peopleNmEn":"YOON Kye-sang","cast":"","castEn":""},{"peopleNm":"김홍파","peopleNmEn":"KIM Hong-pa","cast":"","castEn":""},{"peopleNm":"우현","peopleNmEn":"WOO Hyun","cast":"","castEn":""},{"peopleNm":"김태훈","peopleNmEn":"KIM Tae-hun","cast":"","castEn":""},{"peopleNm":"김선영","peopleNmEn":"KIM Sun-young","cast":"","castEn":""},{"peopleNm":"민진웅","peopleNmEn":"MIN Jin-woong","cast":"","castEn":""}],"showTypes":[{"showTypeGroupNm":"2D","showTypeNm":"디지털"},{"showTypeGroupNm":"2D","showTypeNm":"디지털 영문자막"}],"companys":[{"companyCd":"20124216","companyNm":"더램프(주)","companyNmEn":"The LAMP ltd.","companyPartNm":"제작사"},{"companyCd":"20188021","companyNm":"롯데컬처웍스(주)롯데엔터테인먼트","companyNmEn":"Lotte Cultureworks Lotte Entertainment Co.,Ltd","companyPartNm":"배급사"},{"companyCd":"20188021","companyNm":"롯데컬처웍스(주)롯데엔터테인먼트","companyNmEn":"Lotte Cultureworks Lotte Entertainment Co.,Ltd","companyPartNm":"제공"},{"companyCd":"20188021","companyNm":"롯데컬처웍스(주)롯데엔터테인먼트","companyNmEn":"Lotte Cultureworks Lotte Entertainment Co.,Ltd","companyPartNm":"해외세일즈사"}],"audits":[{"auditNo":"2018-MF02396","watchGradeNm":"12세이상관람가"}],"staffs":[{"peopleNm":"박은경","peopleNmEn":"PARK Eun-kyung","staffRoleNm":"제작"},{"peopleNm":"엄유나","peopleNmEn":"EOM Yuna","staffRoleNm":"시나리오(각본)"},{"peopleNm":"윤서영","peopleNmEn":"","staffRoleNm":"프로듀서"}]},"source":"영화진흥위원회"}}
