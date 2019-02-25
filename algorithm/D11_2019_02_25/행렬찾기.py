ddfb56cd1eab1f4564d03bba4f2eff3a36bed02b
jho0078 <jho0078@gmail.com>
2019-02-26 08:02:01 +0900

    Home | 190226

--- algorithm/D11_2019_02_25/행렬찾기.py
+++ algorithm/D11_2019_02_25/행렬찾기.py
@@ -5,15 +5,10 @@ T = int(input())
 for tc in range(1, T+1):
     n = int(input())
     data = [list(map(int, input().split())) for i in range(n)]
-    # for i in range(n):
-    #     print(data[i])
-
     matrixall = []
 
     for y in range(n):
         for x in range(n):
-            x_length = 1
-            y_length = 1
             y1 = y
             x1 = x
 
@@ -24,29 +19,25 @@ for tc in range(1, T+1):
                 x_start = x1
                 while data[y1][x1] != 0:
                     x1 += 1
-                    x_length += 1
                 x1 -= 1
-                x_length -= 1
 
                 while data[y1][x1] != 0:
                     y1 += 1
-                    y_length += 1
                 y1 -= 1
-                y_length -= 1
 
-                y_end = y1
-                x_end = x1
+                y_length = y1 - y_start + 1
+                x_length = x1 - x_start + 1
 
                 matrix.append(y_length)
                 matrix.append(x_length)
                 matrixall.append(matrix)
 
-                for i in range(y_start, y_end+1):
-                    for j in range(x_start, x_end+1):
+                for i in range(y_start, y1+1):
+                    for j in range(x_start, x1+1):
                         data[i][j] = 0
 
-
     sorted_matrix = sorted(matrixall, key=lambda x: (x[0]*x[1], x[0]))
+
     result = []
     for i in range(len(sorted_matrix)):
         for j in range(2):
