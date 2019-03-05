8498de86041493d54ee629a8de8e6b3a85a6c21c
jho0078 <jho0078@gmail.com>
2019-03-06 03:00:02 +0900

    Home | 190306 | algorithm

--- algorithm/D14_2019_03_01/Snake(코드 줄이기).py
+++ algorithm/D14_2019_03_01/Snake(코드 줄이기).py
@@ -8,10 +8,10 @@ def iswall(y, x):
     else:
         return False
 
+
 N = int(input())
-print(N)
 K = int(input())
-table = [[0 for _ in range(N+1)] for _ in range(N+1)]
+table = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
 
 for i in range(K):
     r, c = map(int, input().split())
@@ -19,16 +19,12 @@ for i in range(K):
 
 L = int(input())
 data = [input().split() for i in range(L)]
-for i in range(N):
-    print(table[i])
-print(data)
 
 x = 1
 y = 1
 i = 0
-move = 0
+time = 0
 idx = 0
-# length = 1
 body = []
 while True:
 
@@ -40,44 +36,29 @@ while True:
     nx = x + dx[i]
 
     if idx < len(data):
-        if move < int(data[idx][0]):
+        if time < int(data[idx][0]):
             if not iswall(ny, nx) and table[ny][nx] != 2:
+                body.insert(0, (y, x))
+                table[y][x] = 2
+                y = ny
+                x = nx
+                time += 1
+
                 if table[ny][nx] == 0:
-                    body.insert(0, (y, x))
-                    table[y][x] = 2
-
-                    print(f'move: {move}')
-                    for j in range(N + 1):
-                        print(table[j])
-
-                    y = ny
-                    x = nx
                     eraser = body.pop(-1)
                     table[eraser[0]][eraser[1]] = 0
-                    move += 1
-                elif table[ny][nx] == 1:
-                    body.insert(0, (y, x))
-                    table[y][x] = 2
-                    y = ny
-                    x = nx
 
-                    print(f'move: {move}')
-                    for j in range(N + 1):
-                        print(table[j])
-
-                    move += 1
-
-                print()
             elif iswall(ny, nx) or table[ny][nx] == 2:
-                print(move+1)
+                print(time + 1)
                 break
 
-        elif move == int(data[idx][0]):
+
+        elif time == int(data[idx][0]):
             if data[idx][1] == 'D':
-                i = (i + 1)%4
+                i = (i + 1) % 4
                 idx += 1
             else:
-                i = (i - 1)%4
+                i = (i - 1) % 4
                 idx += 1
 
     else:
@@ -86,20 +67,12 @@ while True:
             table[y][x] = 2
             y = ny
             x = nx
-            move += 1
+            time += 1
 
             if table[ny][nx] == 0:
                 eraser = body.pop(-1)
                 table[eraser[0]][eraser[1]] = 0
 
         elif iswall(ny, nx) or table[ny][nx] == 2:
-            print(move + 1)
+            print(time + 1)
             break
-
-
-
-
-
-
-
-
