8356bb7c2d98f452c7b98917e6d9037645ff5e81
jho0078 <jho0078@gmail.com>
2019-03-09 22:32:56 +0900

    Home | 190309 | algorithm

---
+++ algorithm/D18_2019_03_08(IM)/버스기사.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open("버스기사_input.txt")
+
+T = int(input())
+for tc in range(a, T+1):
+    N = int(input())
+    table = [list(map(int, input().split())) for i in range(N)]
+
+    find
+
