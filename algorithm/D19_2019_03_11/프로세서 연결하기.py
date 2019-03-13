afcc6fe74916300d3a2d11c2b40b25e7ae618449
jho0078 <jho0078@gmail.com>
2019-03-13 16:50:35 +0900

    D42 | 190313 PM | make gitignore

---
+++ algorithm/D19_2019_03_11/프로세서 연결하기.py
@@ -0,0 +1,47 @@
+import sys
+sys.stdin = open("프로세서 연결하기_input.txt")
+
+# def iswall(y, x):
+#     if y < 0 or x < 0 or x >= N or y >= N:
+#         return True
+#     return False
+
+def connect(y, x, idx):
+
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, -1, 1]
+    for i in range(4):
+        for h in range(1, N):
+            nx = x + dx[i]*h
+            ny = y + dy[i]*h
+            if nx == 0 or ny == 0 or nx == N-1 or ny == N-1:
+                if table[ny][nx] != 1:
+                    core[idx].append([ny, nx])
+                    break
+                else:
+                    break
+            elif table[ny][nx] == 1:
+                break
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    table = [list(map(int, input().split())) for i in range(N)]
+    core = []
+    for i in range(1, N-1):
+        for j in range(1, N-1):
+            if table[i][j] == 1:
+                core.append([i,j])
+
+    print(core)
+    for idx in range(len(core)):
+        connect(core[idx][0], core[idx][1], idx)
+
+    print(core)
+
+    for i in range(len(core)):
+        for j in range(2, len(core[i]-2)):
+            abs(core[i][0] - core[i][j][0]) + abs(core[i][1] - core[i][j][1])
+
+
