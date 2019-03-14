e1f37d18da18787d178609a9615de8f2eada52b8
jho0078 <jho0078@gmail.com>
2019-03-15 01:33:56 +0900

    Home | 190315 | processor

--- algorithm/D19_2019_03_11/프로세서 연결하기4.py
+++ algorithm/D19_2019_03_11/프로세서 연결하기4.py
@@ -1,51 +1,48 @@
 import sys
 sys.stdin = open("프로세서 연결하기_input.txt")
 
-def find(idx, end, length, visited):
-    global max_count, min_length
-    visited = visited[:]
+def find(idx, length, tables):
+    global max_count, min_length, end
 
     if idx == end:
         if length < min_length:
             min_length = length
         return
-        # if count > max_count:
-        #     max_count = count
-        #     min_length = length
-        # elif count == max_count:
-        #     if length < min_length:
-        #         min_length = length
-        # return
 
     else:
-        flag = 0
         y = cores[idx][0]
         x = cores[idx][1]
         dx = [1, -1, 0, 0]
         dy = [0, 0, -1, 1]
+        count = 0
         for i in range(4):
+            flag = 0
+            copytable = [tables[k][:] for k in range(len(tables))]
             for h in range(1, N):
                 nx = x + dx[i] * h
                 ny = y + dy[i] * h
 
-                if visited[ny][nx] == 0 and table[ny][nx] != 1:
+                if copytable[ny][nx] == 0:
                     if nx == 0 or ny == 0 or nx == N - 1 or ny == N - 1:
                         flag = 1
-                        if table[ny][nx] != 1:
+                        if copytable[ny][nx] != 1:
                             for j in range(1, h+1):
-                                visited[y + dy[i] * j][x + dx[i] * j] = 1
+                                copytable[y + dy[i] * j][x + dx[i] * j] = 2
                             break
+                elif copytable[ny][nx] == 1:
+                    count += 1
+                    break
                 else:
                     break
 
             if flag == 1:
-                find(idx+1, end, length+h, visited)
-            # else:
-            #     find(idx+1, end, length, visited)
+                find(idx+1, length+h, copytable)
+        if count == 4:
+            find(idx+1, length, copytable)
 
 
 T = int(input())
-for tc in range(1, 2):
+for tc in range(1, T+1):
     N = int(input())
     table = [list(map(int, input().split())) for i in range(N)]
     cores = []
@@ -54,9 +51,8 @@ for tc in range(1, 2):
             if table[i][j] == 1:
                 cores.append([i, j])
 
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-    max_count = 0
     min_length = 10000
+    end = len(cores)
 
-    find(0, len(cores), 0, visited)
-    print(max_count, min_length)
+    find(0, 0, table)
+    print("#{} {}".format(tc, min_length))
